f679c58d124b4c9fe5ec27fad2f7cc4fc697c790
Willie Fu <willie.fu@gmail.com>
2014-05-13 12:37:19 -0700

    Search edits

--- wp-content/themes/holly/searchform.php
+++ wp-content/themes/holly/searchform.php
@@ -1,5 +1,17 @@
-	<form id="search-form-cb" method="POST" action="<?php home_url() ?>/search/" 
-	onsubmit="this.action += filterq()" >
+		<script type="text/javascript">
+		function filterq() {
+			// get our field
+			var s = document.getElementById('s');
+			// our field's value
+			var sv = s.value;
+
+			sv = sv.replace(/[^\w-',"\x20 _]+/, '').trim();
+			s.value = sv;
+			return encodeURIComponent(sv); // handle space and other stuff
+		}
+		</script>
+
+	<form id="search-form-cb" method="POST" action="<?php echo home_url() ?>/search/" onsubmit="this.action += filterq()" >
 		<div class="search-wrapper">
 			<div class="search-wrapper-border">
 				<div class="search-wrapper-inner">
@@ -9,8 +21,7 @@
 					if(empty($s)) $s = $default_val;
 					?>
 					<input type="image" class="floatR right search-submit" id="searchSubmit" name="searchSubmit" value="Search" src="wp-content/themes/holly/images/search-widget-btn.png" />
-					<input type="text" id="s" class="inputText" name="s" value="<?php $s?>"
-							onfocus="if(this.value==this.defaultValue)this.value='';" onblur="if(this.value=='')this.value=this.defaultValue;" />
+					<input type="text" id="s" class="inputText" name="s" value="<?php echo $s?>" onfocus="if(this.value==this.defaultValue)this.value='';" onblur="if(this.value=='')this.value=this.defaultValue;" />
 				</div>
 			</div>
 		</div>
